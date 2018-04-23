15ba5dc27888a8177ded9861185771543ab14fbe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AnimateChangedRows/Window1.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+using DevExpress.Xpf.Grid;
+
+namespace AnimateChangedRows {
+    public partial class Window1 : Window {
+        BindingList<TestData> list = new BindingList<TestData>();
+
+        Dictionary<int, AnimationElement> animationElements =
+            new Dictionary<int, AnimationElement>();
+
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public Window1() {
+            InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+                list.Add(new TestData() { Text = "Element" + i.ToString(), Number = i });
+            grid.ItemsSource = list;
+
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.IsEnabled = true;
+        }
+
+        Random randow = new Random();
+
+        void timer_Tick(object sender, EventArgs e) {
+            int rowHandle = grid.GetRowHandleByListIndex(randow.Next(list.Count));
+            grid.SetCellValue(rowHandle, grid.Columns[1], randow.Next(1000));
+        }
+
+        private void grid_CustomUnboundColumnData(object sender, GridColumnDataEventArgs e) {
+            if (e.Column.FieldName == "AnimationElement") {
+                e.Value = GetAnimationElement(e.ListSourceRowIndex);
+            }
+        }
+
+        AnimationElement GetAnimationElement(int listIndex) {
+            AnimationElement element = new AnimationElement();
+            bool flag = animationElements.TryGetValue(listIndex, out element);
+            if (!flag) {
+                element = new AnimationElement();
+                animationElements[listIndex] = element;
+            }
+            return element;
+        }
+
+        private void view_CellValueChanged(object sender, CellValueEventArgs e) {
+            GetAnimationElement(grid.GetListIndexByRowHandle(e.RowHandle)).StartAnimation();
+        }
+    }
+
+    public class TestData : INotifyPropertyChanged {
+        int number;
+        string text;
+        public int Number {
+            get { return number; }
+            set {
+                if (number == value)
+                    return;
+                number = value;
+                NotifyChanged("Number");
+            }
+        }
+        public string Text {
+            get { return text; }
+            set {
+                if (text == value)
+                    return;
+                text = value;
+                NotifyChanged("Text");
+            }
+        }
+        void NotifyChanged(string propertyName) {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+
+    public class AnimationElement : FrameworkContentElement {
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(Color), typeof(AnimationElement),
+            new PropertyMetadata(Colors.White, OnChanged));
+        static void OnChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        }
+        public Color Color {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public void StartAnimation() {
+            ColorAnimationUsingKeyFrames animation =
+                new ColorAnimationUsingKeyFrames();
+            animation.KeyFrames.Add(new DiscreteColorKeyFrame(Color.FromArgb(255, 255, 255, 255),
+                KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0))));
+            animation.KeyFrames.Add(new LinearColorKeyFrame(Color.FromArgb(255, 200, 200, 200),
+                KeyTime.FromTimeSpan(TimeSpan.FromSeconds(1))));
+            animation.KeyFrames.Add(new LinearColorKeyFrame(Color.FromArgb(255, 255, 255, 255),
+                KeyTime.FromTimeSpan(TimeSpan.FromSeconds(1.5))));
+
+            this.BeginAnimation(AnimationElement.ColorProperty, animation,
+                HandoffBehavior.SnapshotAndReplace);
+        }
+    }
+
+    public class ColorToBrushConverter : MarkupExtension, IValueConverter {
+        object IValueConverter.Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture) {
+            return new SolidColorBrush((Color)value);
+        }
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture) {
+            return value;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to highlight modified rows
+
+
+<p>Our controls provide the built-in way to show the current <a href="https://documentation.devexpress.com/WPF/17130/Controls-and-Libraries/Data-Grid/Conditional-Formatting">Conditional Formatting</a> with an animated effect. All you need is to apply the required conditional formatting and then set the <a href="https://documentation.devexpress.com/WPF/DevExpress.Xpf.Grid.TableView.AnimateConditionalFormattingTransition.property">TableView.AnimateConditionalFormattingTransition</a> property to True.</p><p><br><em>For versions prior to 17.1:</em><br>The grid's data source is changed in the background. A modified grid row flashes and then fades out. Download the example, to learn how this functionality is implemented.</p><p><strong>See Also:</strong><br> <a href="https://www.devexpress.com/Support/Center/p/E1297">How to change background color for modified cells</a><br> <a href="https://www.devexpress.com/Support/Center/p/E1532">How to embed animation into grid cells</a></p>
+
+<br/>
+
+
