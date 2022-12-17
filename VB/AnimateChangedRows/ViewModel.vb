f5156a4739fa3e6e5f098d46c191e6531aaf9815
DevExpressExampleBot <fakeEmail>
2022-12-17 04:57:54 +0400

    Source auto update [skip ci]

--- VB/AnimateChangedRows/ViewModel.vb
+++ VB/AnimateChangedRows/ViewModel.vb
@@ -1,33 +1,31 @@
-﻿Imports DevExpress.Xpf.Grid
 Imports System
-Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 Imports System.ComponentModel
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Threading
 
 Namespace AnimateChangedRows
+
     Public Class ViewModel
 
-        Public Property List() As ObservableCollection(Of TestData)
-        Private timer As New DispatcherTimer()
-        Private random As New Random()
+        Public Property List As ObservableCollection(Of TestData)
 
+        Private timer As DispatcherTimer = New DispatcherTimer()
+
+        Private random As Random = New Random()
 
         Public Sub New()
             List = New ObservableCollection(Of TestData)()
             PopulateData()
-            AddHandler timer.Tick, AddressOf timer_Tick
+            AddHandler timer.Tick, New EventHandler(AddressOf timer_Tick)
             timer.Interval = TimeSpan.FromMilliseconds(100)
             timer.IsEnabled = True
         End Sub
 
         Private Sub PopulateData()
-            For i As Integer = 0 To 29
+            For i As Integer = 0 To 30 - 1
                 Dim t = New TestData(i, "Element" & i.ToString())
                 List.Add(t)
-            Next i
+            Next
         End Sub
 
         Private Sub timer_Tick(ByVal sender As Object, ByVal e As EventArgs)
@@ -39,48 +37,45 @@ Namespace AnimateChangedRows
     Public Class TestData
         Implements INotifyPropertyChanged
 
+        Private numberField As Integer
 
-        Private number_Renamed As Integer
-
-        Private text_Renamed As String
+        Private textField As String
 
         Public Sub New(ByVal number As Integer, ByVal text As String)
-            Me.number_Renamed = number
-            Me.text_Renamed = text
+            numberField = number
+            textField = text
         End Sub
 
-
-
-        Public Property Number() As Integer
+        Public Property Number As Integer
             Get
-                Return number_Renamed
+                Return numberField
             End Get
+
             Set(ByVal value As Integer)
-                If number_Renamed = value Then
-                    Return
-                End If
-                number_Renamed = value
+                If numberField = value Then Return
+                numberField = value
                 NotifyChanged("Number")
             End Set
         End Property
-        Public Property Text() As String
+
+        Public Property Text As String
             Get
-                Return text_Renamed
+                Return textField
             End Get
+
             Set(ByVal value As String)
-                If text_Renamed = value Then
-                    Return
-                End If
-                text_Renamed = value
+                If Equals(textField, value) Then Return
+                textField = value
                 NotifyChanged("Text")
             End Set
         End Property
+
         Private Sub NotifyChanged(ByVal propertyName As String)
             RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
         End Sub
 
-        #Region "INotifyPropertyChanged Members"
+#Region "INotifyPropertyChanged Members"
         Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
-        #End Region
+#End Region
     End Class
 End Namespace
--- VB/AnimateChangedRows/Window1.xaml.vb
+++ VB/AnimateChangedRows/Window1.xaml.vb
@@ -1,22 +1,13 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Windows
-Imports System.Windows.Data
-Imports System.Windows.Markup
-Imports System.Windows.Media
-Imports System.Windows.Media.Animation
-Imports System.Windows.Threading
-Imports DevExpress.Xpf.Grid
-Imports System.Collections.ObjectModel
 
 Namespace AnimateChangedRows
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
-
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 End Namespace
